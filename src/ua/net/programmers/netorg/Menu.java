03db055578b87ec1f6bff11308fb2fd031c32cf5
Bogdan Levkiv <levkiv.b@gmail.com>
2015-01-20 22:41:16 +0200

    add source

---
+++ src/ua/net/programmers/netorg/Menu.java
@@ -0,0 +1,16 @@
+package ua.at.programmers.netorg;
+
+public class Menu {
+    public MenuFormMain() {
+        System.out.println("1. Open port");
+        System.out.println("2. Close port");
+        System.out.println("3. Write to port");
+        System.out.println("4. Read from port");
+    }
+    public MenuForm2() {
+        System.out.println("1. Open port");
+        System.out.println("2. Close port");
+        System.out.println("3. Write to port");
+        System.out.println("4. Read from port");
+    }
+}
